34a237e23f50ed4ab1c23d6878a722fcd7b9caa3
newt239 <ysti2019.win2020.st@gmail.com>
2022-01-02 13:41:13 +0900

    improve: after scan

--- src/components/page/Exhibit/Pass.tsx
+++ src/components/page/Exhibit/Pass.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../stores/index';
 import { Grid, Typography } from '@mui/material';
 
 import ExhibitScan from '../../ui/ExhibitScan';
 
 export default function ExhibitPass() {
+    const exhibit = useSelector((state: RootState) => state.exhibit);
     return (
         <Grid container spacing={{ xs: 2, md: 3 }}>
             <Grid item xs={12}>
-                <Typography variant='h3'>通過を記録するリストバンドのQRコードをかざしてください</Typography>
+                <Typography variant='h3'>{exhibit.current.exhibit_name}</Typography>
             </Grid>
             <ExhibitScan scanType="pass" />
         </Grid>
--- src/components/ui/ExhibitScan.tsx
+++ src/components/ui/ExhibitScan.tsx
@@ -1,42 +1,57 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import { useDispatch } from 'react-redux';
 import store from '../../stores/index';
 import { pauseQrReader } from '../../stores/scan';
-import { Grid, Typography, Button, FormControl, InputLabel, IconButton, InputAdornment, OutlinedInput, Box, LinearProgress, Card, List, ListItem, ListItemIcon, ListItemText, Snackbar } from '@mui/material';
-import Scanner from '../ui/Scanner';
-import axios from 'axios';
-import PermIdentityRoundedIcon from '@mui/icons-material/PermIdentityRounded';
+
+import { Slide, Grid, Dialog, Typography, Button, FormControl, IconButton, InputAdornment, OutlinedInput, Box, LinearProgress, Card, List, ListItem, ListItemIcon, ListItemText, Snackbar } from '@mui/material';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
+import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
+import PeopleRoundedIcon from '@mui/icons-material/PeopleRounded';
 import AccessTimeRoundedIcon from '@mui/icons-material/AccessTimeRounded';
 import ContentCopyRoundedIcon from '@mui/icons-material/ContentCopyRounded';
+import { TransitionProps } from '@mui/material/transitions';
 
-import SelectExhibit from './SelectExhibit';
+import Scanner from '../ui/Scanner';
 
 const API_BASE_URL: string = process.env.REACT_APP_API_BASE_URL!;
 
 type ExhibitScanProps = {
     scanType: string;
-}
+};
+type guestInfoProp = {
+    guest_id: string;
+    guest_type: string;
+    exhibit_id: string;
+    part: string;
+    available: false;
+    note: string;
+} | null;
+const Transition = React.forwardRef(function Transition(
+    props: TransitionProps & {
+        children: React.ReactElement;
+    },
+    ref: React.Ref<unknown>,
+) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
 
 const ExhibitScan: React.FunctionComponent<ExhibitScanProps> = ({ scanType }) => {
+    const theme = useTheme();
+    const matches = useMediaQuery(theme.breakpoints.up('sm'));
     const dispatch = useDispatch();
     const token = store.getState().auth.token;
     const user = store.getState().user;
     const exhibit = store.getState().exhibit;
     const [text, setText] = useState<string>("");
+    const [scanStatus, setScanStatus] = useState<"waiting" | "success" | "error">("waiting");
+    const [message, setMessage] = useState<string[]>([]);
     const [loading, setLoading] = useState<boolean>(false);
-    const [guestInfo, setGuestInfo] = useState({
-        guest_id: "",
-        guest_type: "",
-        reservation_id: "",
-        exhibit_id: "",
-        part: "",
-        available: false,
-        note: "",
-        regist_at: ""
-    });
-    const [message, setMessage] = useState({ type: "beforeScan", title: "" });
+    const [guestInfo, setGuestInfo] = useState<guestInfoProp>(null);
     const [disabled, setButtonStatus] = useState<boolean>(true);
     const [copySnack, setCopySnack] = useState(false);
+    const [smDialogOpen, setSmDialogStatus] = useState(false);
     const handleScan = async (scanText: string | null) => {
         if (scanText) {
             if (scanText.length === 10 && scanText.startsWith('G')) {
@@ -49,12 +64,20 @@ const ExhibitScan: React.FunctionComponent<ExhibitScanProps> = ({ scanType }) =>
                     setGuestInfo(res.data.data);
                     const guestData = res.data.data;
                     if (!guestData.available) {
-                        setMessage({ type: "scanError", title: "このゲストは無効です" });
+                        setMessage(["このゲストは無効です。"]);
                     } else {
-                        setMessage({ type: "scanSuccess", title: `スキャンに成功しました` });
+                        setScanStatus("success");
+                        setMessage(["スキャンに成功しました。"]);
+                        setSmDialogStatus(true);
                     }
+                } else {
+                    setScanStatus("error");
+                    setMessage(["何らかの問題が発生しました。"]);
                 };
                 setButtonStatus(false);
+            } else {
+                setScanStatus("error");
+                setMessage(["ゲストidの形式が正しくありません。"]);
             }
         }
     };
@@ -65,20 +88,65 @@ const ExhibitScan: React.FunctionComponent<ExhibitScanProps> = ({ scanType }) =>
         setCopySnack(false);
     };
     const postApi = async () => {
-        const payload = {
-            guest_id: text,
-            guest_type: guestInfo.guest_type,
-            exhibit_id: exhibit.current.exhibit_id,
-            userid: user.userid
+        if (guestInfo) {
+            const payload = {
+                guest_id: text,
+                guest_type: guestInfo.guest_type,
+                exhibit_id: exhibit.current.exhibit_id,
+                userid: user.userid
+            };
+            const res = await axios.post(`${API_BASE_URL}/v1/activity/${scanType}`, payload, { headers: { Authorization: "Bearer " + token } }).then(res => { return res });
+            dispatch(pauseQrReader(true));
+            console.log(res);
         };
-        const res = await axios.post(`${API_BASE_URL}/v1/activity/${scanType}`, payload, { headers: { Authorization: "Bearer " + token } }).then(res => { return res });
-        dispatch(pauseQrReader(true));
-        console.log(res);
     };
     const retry = () => {
         dispatch(pauseQrReader(true));
         setText("");
-        setMessage({ type: "beforeScan", title: "スキャンしてください" });
+        setMessage([]);
+    };
+    const GuestInfoCard = () => {
+        return (
+            <>
+                {guestInfo && (
+                    <Card variant="outlined" sx={{ p: 2 }} >
+                        <Typography variant="h4">ゲスト情報</Typography>
+                        <List dense>
+                            <ListItem>
+                                <ListItemIcon>
+                                    <PersonRoundedIcon />
+                                </ListItemIcon>
+                                <ListItemText
+                                    primary={text}
+                                />
+                            </ListItem>
+                            <ListItem>
+                                <ListItemIcon>
+                                    <PeopleRoundedIcon />
+                                </ListItemIcon>
+                                <ListItemText
+                                    primary={guestInfo.guest_type === "student" ? "生徒" : guestInfo.guest_type}
+                                />
+                            </ListItem>
+                            <ListItem>
+                                <ListItemIcon>
+                                    <AccessTimeRoundedIcon />
+                                </ListItemIcon>
+                                <ListItemText
+                                    primary={guestInfo.part === "all" ? "全時間帯" : guestInfo.part}
+                                />
+                            </ListItem>
+                        </List>
+                        <Box
+                            m={1}
+                            sx={{ display: 'flex', justifyContent: "flex-end", alignItems: "flex-end", gap: "1rem" }}>
+                            <Button variant="outlined" onClick={retry}>スキャンし直す</Button>
+                            <Button variant="contained" onClick={postApi} disabled={disabled}>登録</Button>
+                        </Box>
+                    </Card >
+                )}
+            </>
+        )
     }
     return (
         <>
@@ -87,10 +155,11 @@ const ExhibitScan: React.FunctionComponent<ExhibitScanProps> = ({ scanType }) =>
             </Grid>
             <Grid item xs={12} md={6}>
                 <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-                    <Typography variant='h4' sx={{ minWidth: '7rem' }}>認識したid</Typography>
+                    <Typography variant='h4'>id:</Typography>
                     <FormControl sx={{ m: 1 }} variant="outlined">
                         <OutlinedInput
                             type='text'
+                            size="small"
                             value={text}
                             onChange={(e) => setText(e.target.value)}
                             endAdornment={
@@ -116,39 +185,20 @@ const ExhibitScan: React.FunctionComponent<ExhibitScanProps> = ({ scanType }) =>
                 {loading && (<Box sx={{ width: '100%' }}>
                     <LinearProgress />
                 </Box>)}
-                {message.type === "scanSuccess" && (
-                    <Card variant="outlined" sx={{ p: 2 }}>
-                        <Typography variant="h4">ゲスト情報</Typography>
-                        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-                            <Typography variant='h4' sx={{ minWidth: '7rem' }}>展示名</Typography>
-                            <SelectExhibit />
-                        </Box>
-                        <List dense>
-                            <ListItem>
-                                <ListItemIcon>
-                                    <PermIdentityRoundedIcon />
-                                </ListItemIcon>
-                                <ListItemText
-                                    primary={guestInfo.guest_type === "student" ? "生徒" : guestInfo.guest_type}
-                                />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemIcon>
-                                    <AccessTimeRoundedIcon />
-                                </ListItemIcon>
-                                <ListItemText
-                                    primary={guestInfo.part === "all" ? "全時間帯" : guestInfo.part}
-                                />
-                            </ListItem>
-                        </List>
-                        <Box
-                            m={1}
-                            sx={{ display: 'flex', justifyContent: "flex-end", alignItems: "flex-end", gap: "1rem" }}>
-                            <Button variant="outlined" onClick={retry}>スキャンし直す</Button>
-                            <Button variant="contained" onClick={postApi} disabled={disabled}>登録</Button>
-                        </Box>
-                    </Card>)
-                }
+                {scanStatus === "success" && (
+                    matches ? (
+                        <GuestInfoCard />
+                    ) : (
+                        <Dialog
+                            fullScreen
+                            open={smDialogOpen}
+                            onClose={handleClose}
+                            TransitionComponent={Transition}
+                        >
+                            <GuestInfoCard />
+                        </Dialog>
+                    )
+                )}
             </Grid >
         </>
     );
