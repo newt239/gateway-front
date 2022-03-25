5902b2e4f540ba4cc4e4e67436ac0572b3acfc92
newt239 <ysti2019.win2020.st@gmail.com>
2022-03-25 18:51:02 +0900

    change: display data per date at ExhibitEnterCountBarChart

--- src/components/block/ExhibitEnterCountBarChart.tsx
+++ src/components/block/ExhibitEnterCountBarChart.tsx
@@ -7,39 +7,41 @@ import Chart from "react-apexcharts";
 import { ApexOptions } from "apexcharts";
 import moment from "moment";
 
+import { TextField } from '@mui/material';
+
 const API_BASE_URL: string = process.env.REACT_APP_API_BASE_URL!;
 
 const ExhibitEnterCountBarChart: React.FunctionComponent<{ exhibit_id: string; }> = ({ exhibit_id }) => {
     const token = useRecoilValue(tokenState);
     const [categories, setCategories] = useState<string[]>([]);
     const [data, setData] = useState<number[]>([]);
+    const [day, setDay] = useState(moment().format("YYYY-MM-DD"));
     useEffect(() => {
-        if (token) {
-            const getApi = async () => {
-                const res = await axios.get(`${API_BASE_URL}/v1/exhibit/enter-chart/${exhibit_id}`, { headers: { Authorization: "Bearer " + token } }).then(res => { return res });
-                if (res.data.status === "success" && res.data.data.length !== 0) {
-                    const rawData: { time: string; count: number; }[] = res.data.data;
-                    let timeList: string[] = [];
-                    let countList: number[] = [];
-                    let ctime = moment(rawData[0].time);
-                    for (const eachData of rawData) {
-                        const eachTime = moment(eachData.time);
-                        while (ctime < eachTime) {
-                            timeList.push(ctime.format("MM/DD HH:MM:SS"));
-                            countList.push(0);
-                            ctime = ctime.add(1, "hours");
-                        }
-                        timeList.push(eachTime.format("MM/DD HH:MM:SS"));
-                        countList.push(eachData.count);
-                    };
-                    setCategories(timeList);
-                    setData(countList);
-                    console.log(countList);
+        const getApi = async () => {
+            const res = await axios.get(`${API_BASE_URL}/v1/exhibit/enter-chart/${exhibit_id}?day=${day}`, { headers: { Authorization: "Bearer " + token } }).then(res => { return res });
+            console.log(res);
+            if (res.data.status === "success" && res.data.data.length !== 0) {
+                const rawData: { time: string; count: number; }[] = res.data.data;
+                let timeList: string[] = [];
+                let countList: number[] = [];
+                let ctime = moment(rawData[0].time);
+                for (const eachData of rawData) {
+                    const eachTime = moment(eachData.time);
+                    while (ctime < eachTime) {
+                        timeList.push(ctime.format("MM/DD HH:MM:SS"));
+                        countList.push(0);
+                        ctime = ctime.add(1, "hours");
+                    }
+                    timeList.push(eachTime.format("MM/DD HH:MM:SS"));
+                    countList.push(eachData.count);
                 };
+                setCategories(timeList);
+                setData(countList);
+                console.log(countList);
             };
-            getApi();
         };
-    }, [token]);
+        getApi();
+    }, [day]);
     const options: ApexOptions = {
         chart: {
             type: "bar",
@@ -71,6 +73,9 @@ const ExhibitEnterCountBarChart: React.FunctionComponent<{ exhibit_id: string; }
             x: {
                 format: "MM/dd HH:00"
             }
+        },
+        noData: {
+            text: "表示するデータがありません"
         }
     };
     const series = [
@@ -81,6 +86,14 @@ const ExhibitEnterCountBarChart: React.FunctionComponent<{ exhibit_id: string; }
     ];
     return (
         <>
+            <TextField
+                id="date"
+                label="選択中の日"
+                type="date"
+                defaultValue={day}
+                onChange={e => setDay(e.target.value)}
+                margin='normal'
+            />
             <Chart
                 options={options}
                 series={series}
