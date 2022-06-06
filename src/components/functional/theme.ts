4cd2dd6672a72ff1638432353fdfcc16a8826853
newt239 <ysti2019.win2020.st@gmail.com>
2022-06-06 16:17:14 +0900

    update: favicon

--- src/components/functional/theme.ts
+++ src/components/functional/theme.ts
@@ -1,18 +1,10 @@
 import { createTheme } from "@mui/material/styles";
+import { blue } from "@mui/material/colors";
 
 const theme = createTheme({
   palette: {
     primary: {
-      light: '#757ce8',
-      main: '#3f50b5',
-      dark: '#002884',
-      contrastText: '#fff',
-    },
-    secondary: {
-      light: '#ff7961',
-      main: '#f44336',
-      dark: '#ba000d',
-      contrastText: '#000',
+      main: blue[500],
     },
   },
   typography: {
