85a0af773cf9309a1edafbda1441098a8862707d
newt239 <ysti2019.win2020.st@gmail.com>
2022-06-11 21:30:57 +0900

    change: replace from Autocomplete to Select at SelectExhibit component

--- src/components/block/SelectExhibit.tsx
+++ src/components/block/SelectExhibit.tsx
@@ -2,44 +2,39 @@ import React from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { currentExhibitState, exhibitListState } from "#/recoil/exhibit";
 
-import { Stack, FormControl, TextField, Autocomplete } from "@mui/material";
+import { FormControl, Select, SelectChangeEvent, MenuItem } from "@mui/material";
 
 const SelectExhibit: React.FunctionComponent<{
   disabled?: boolean | false;
 }> = ({ disabled }) => {
 
-  const [currentExhibit, setCurrentExhibit] =
-    useRecoilState(currentExhibitState);
+  const [currentExhibit, setCurrentExhibit] = useRecoilState(currentExhibitState);
   const exhibitList = useRecoilValue(exhibitListState);
 
-  const handleChange = (
-    event: React.SyntheticEvent,
-    value: { exhibit_id: string; exhibit_name: string },
-    reason: string
-  ) => {
-    if (reason === "clear") {
-      setCurrentExhibit({ exhibit_id: "", exhibit_name: "" });
-    } else {
-      setCurrentExhibit(value);
+  const handleChange = (event: SelectChangeEvent) => {
+    const newCurrentExhibit = exhibitList.find(v => {
+      if (v.exhibit_id === event.target.value) {
+        return v
+      }
+    })
+    if (newCurrentExhibit) {
+      return setCurrentExhibit(newCurrentExhibit);
     }
+    setCurrentExhibit({ exhibit_id: "", exhibit_name: "" });
   };
 
   return (
     <FormControl sx={{ m: 1, minWidth: 200 }}>
-      <Stack spacing={2}>
-        <Autocomplete
-          disableClearable
-          disabled={disabled}
-          size="small"
-          options={exhibitList}
-          getOptionLabel={(option) => option.exhibit_name}
-          value={currentExhibit}
-          onChange={handleChange}
-          renderInput={(params) => (
-            <TextField {...params} label="展示名" placeholder="展示名" />
-          )}
-        />
-      </Stack>
+      <Select
+        disabled={disabled}
+        size="small"
+        value={currentExhibit.exhibit_id}
+        onChange={handleChange}
+      >
+        {exhibitList.map(v => {
+          return <MenuItem value={v.exhibit_id} key={v.exhibit_id}>{v.exhibit_name}</MenuItem>
+        })}
+      </Select>
     </FormControl>
   );
 };
