3289c7295f63bb2dcfe40ba13b1577267ef8626e
newt239 <ysti2019.win2020.st@gmail.com>
2022-07-13 22:07:37 +0900

    improve: NumPad responsive style

--- src/components/block/NumPad.tsx
+++ src/components/block/NumPad.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import { Box, Button, Dialog, DialogContent, DialogTitle, Fab, Grid } from "@mui/material";
+import { Box, Button, Dialog, DialogContent, DialogTitle, Fab, Grid, ButtonGroup, DialogContentText, DialogActions } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import ModeEditRoundedIcon from '@mui/icons-material/ModeEditRounded';
 import BackspaceRoundedIcon from '@mui/icons-material/BackspaceRounded';
 
@@ -8,6 +10,9 @@ const NumPad = ({ scanType, onClose }: { scanType: "reservation" | "guest", onCl
   const [open, setOpen] = useState(false);
   const [id, setId] = useState<number[]>([]);
 
+  const theme = useTheme();
+  const md = useMediaQuery(theme.breakpoints.down("md"));
+  console.log(md)
   const openNumPad = () => {
     setOpen(true);
   };
@@ -40,34 +45,46 @@ const NumPad = ({ scanType, onClose }: { scanType: "reservation" | "guest", onCl
         open={open}
         onClose={handleClose}
         maxWidth="xs"
+        fullScreen={md}
       >
-        <DialogTitle sx={{ textAlign: "center" }}>
-          {scanType === "reservation" ? "R" : "G"}
-          - {id.map(n => <span key={n}>{n}</span>)}</DialogTitle>
-        <DialogContent sx={{ padding: "20px 5rem" }}>
-          <Grid container sx={{ gap: "1rem", justifyContent: "space-between" }}>
-            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map(n => {
-              return (
-                <Grid item key={String(n)}>
-                  <Button variant="outlined" onClick={() => onNumClick(n)}>{n}</Button>
-                </Grid>
-              )
-            })}
-            <Grid item>
-              <Button variant="outlined" onClick={onDeleteNum} disabled={id.length <= 0}>
-                <BackspaceRoundedIcon />
-              </Button >
+        <Box sx={{ display: "grid", alignContent: "space-between", height: "100%" }}>
+          <DialogTitle>
+            ID手動入力
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText sx={{ textAlign: "center", fontWeight: 800 }}>
+              {scanType === "reservation" ? "R" : "G"}
+              <span style={{ padding: "0 .5rem" }}>-</span>
+              <ButtonGroup variant="outlined">
+                {(scanType === "reservation" ? [0, 1, 2, 3, 4, 5, 6] : [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]).map(i => {
+                  return <Button disabled sx={{ fontWeight: 800, "&.Mui-disabled": { color: "black" } }} key={i}>{i < id.length ? id[i] : "_"}</Button>;
+                })}
+              </ButtonGroup>
+            </DialogContentText>
+            <Grid container sx={{ padding: ".5rem", width: 300, margin: "1rem auto 0", gap: "1rem", justifyContent: "space-between" }}>
+              {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map(n => {
+                return (
+                  <Grid item key={String(n)}>
+                    <Button variant="outlined" onClick={() => onNumClick(n)}>{n}</Button>
+                  </Grid>
+                )
+              })}
+              <Grid item>
+                <Button variant="outlined" onClick={onDeleteNum} disabled={id.length <= 0}>
+                  <BackspaceRoundedIcon />
+                </Button >
+              </Grid>
             </Grid>
-          </Grid>
-          <Box sx={{ width: "100%", textAlign: "right" }}>
-            <Button
-              onClick={handleClose}
-              sx={{ mt: 2 }}
-            >
-              閉じる
-            </Button>
-          </Box>
-        </DialogContent>
+            <DialogActions>
+              <Button
+                onClick={handleClose}
+                sx={{ mt: 2 }}
+              >
+                閉じる
+              </Button>
+            </DialogActions>
+          </DialogContent>
+        </Box>
       </Dialog>
     </>
   );
