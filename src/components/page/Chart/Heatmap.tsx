bce323777a1d143ca8d2325a190fa3b92be0d381
newt239 <ysti2019.win2020.st@gmail.com>
2022-07-16 23:14:26 +0900

    change: mounting method of heatmap

--- src/components/page/Chart/Heatmap.tsx
+++ src/components/page/Chart/Heatmap.tsx
@@ -1,8 +1,12 @@
-import React, { useEffect } from "react";
-import { useSetRecoilState } from "recoil";
+import React, { useEffect, useState } from "react";
+import { useSetRecoilState, useRecoilValue } from "recoil";
 import { pageStateSelector } from "#/recoil/page";
+import { tokenState } from "#/recoil/user";
 
-import { Box } from "@mui/material";
+import { Grid, List, ListItem, ListItemText, Typography } from "@mui/material";
+import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
+import { AxiosError } from "axios";
+import apiClient from "#/axios-config";
 
 const Heatmap = () => {
   const setPageInfo = useSetRecoilState(pageStateSelector);
@@ -10,40 +14,62 @@ const Heatmap = () => {
     setPageInfo({ title: "ヒートマップ" });
   }, []);
 
-  type propTypes = {
-    exhibit_id: string;
-    room_name: string;
-    x: number;
-    y: number;
-    width: number;
-    height: number;
-  }
-  const exhibitList: propTypes[] = [
-    { exhibit_id: "1", room_name: "1201", x: 1000, y: 100, width: 50, height: 50 },
-    { exhibit_id: "2", room_name: "1202", x: 940, y: 100, width: 50, height: 50 },
-    { exhibit_id: "3", room_name: "1203", x: 880, y: 100, width: 50, height: 50 },
-    { exhibit_id: "4", room_name: "1204", x: 780, y: 80, width: 50, height: 50 },
-    { exhibit_id: "5", room_name: "1205", x: 780, y: 140, width: 50, height: 50 },
-    { exhibit_id: "6", room_name: "1206", x: 780, y: 200, width: 50, height: 50 },
-    { exhibit_id: "7", room_name: "1207", x: 780, y: 260, width: 50, height: 50 },
-    { exhibit_id: "8", room_name: "1208", x: 780, y: 320, width: 50, height: 50 }
-  ]
+  const token = useRecoilValue(tokenState);
 
-  const EachExhibit = ({ posProps }: { posProps: propTypes }) => {
-    return (<svg width={posProps.width} height={posProps.height} xmlns="http://www.w3.org/2000/svg" style={{ position: "fixed", top: posProps.y, left: posProps.x }}>
-      <rect width={posProps.width} height={posProps.height} rx="10" ry="10" fill="#e74c3c" />
-      <text x="50%" y="50%" fontFamily="serif" fontWeight="bold" fontSize={15} textAnchor="middle" dominantBaseline="middle" fill="#fff">
-        {posProps.room_name}
-      </text>
-    </svg>)
-  }
+  type exhibitProp = {
+    id: string;
+    exhibit_name: string;
+    count: number;
+    capacity: number;
+  };
+
+  const [floor1List, setFloor1List] = useState<exhibitProp[]>([]);
+
+  useEffect(() => {
+    if (token) {
+      apiClient(process.env.REACT_APP_API_BASE_URL)
+        .exhibit.current.$get({
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        .then((res) => {
+          setFloor1List(res);
+        })
+        .catch((err: AxiosError) => {
+          console.log(err);
+        });
+    }
+  }, [token]);
+
+  const ExhibitListBlock = ({ exhibit }: { exhibit: exhibitProp }) => {
+    return (
+      <ListItem divider sx={{ flexDirection: "column" }}>
+        <Grid container sx={{ alignItems: "center", justifyContent: "space-between" }}>
+          <Grid item><ListItemText>{exhibit.exhibit_name}</ListItemText></Grid>
+          <Grid item><span style={{ fontSize: "2rem", fontWeight: 800 }}>{exhibit.count}</span> / {exhibit.capacity} 人</Grid>
+        </Grid>
+        <LinearProgress variant="determinate" value={exhibit.count / exhibit.capacity * 100} sx={{
+          width: "100%", height: 10, [`&.${linearProgressClasses.colorPrimary}`]: {
+            backgroundColor: "transparent",
+            borderRadius: 5
+          },
+          [`& .${linearProgressClasses.bar}`]: {
+            borderRadius: 5,
+            backgroundColor: '#1a90ff',
+          },
+        }} />
+      </ListItem>
+    )
+  };
 
   return (
-    <Box sx={{ p: 2, backgroundColor: "white", zIndex: 1200, position: "fixed", top: 0, left: 0, width: "100%", height: "100%" }}>
-      {exhibitList.map((eachExhibit) => {
-        return <EachExhibit posProps={eachExhibit} key={eachExhibit.exhibit_id} />
-      })}
-    </Box>
+    <Grid container spacing={2} sx={{ p: 2 }}>
+      <Grid item xs={12} md={6}>
+        <Typography variant="h3">Floor 1</Typography>
+        <List>
+          {floor1List.map((exhibit) => (<ExhibitListBlock key={exhibit.id} exhibit={exhibit} />))}
+        </List>
+      </Grid>
+    </Grid>
   );
 };
 
