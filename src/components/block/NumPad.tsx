2faa334f79327b0094f03004a9730bd226007488
newt239 <ysti2019.win2020.st@gmail.com>
2022-07-17 12:13:03 +0900

    fix: numpad length at reservation scan

--- src/components/block/NumPad.tsx
+++ src/components/block/NumPad.tsx
@@ -79,7 +79,7 @@ const NumPad = ({
               <span style={{ padding: "0 .5rem" }}>-</span>
               <ButtonGroup variant="outlined">
                 {(scanType === "reservation"
-                  ? [0, 1, 2, 3, 4, 5, 6]
+                  ? [0, 1, 2, 3, 4, 5]
                   : [0, 1, 2, 3, 4, 5, 6, 7, 8]
                 ).map((i) => {
                   return (
