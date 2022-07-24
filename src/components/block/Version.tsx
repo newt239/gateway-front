d777b7c22e7bbab79702644221cdc408c97885ee
newt239 <ysti2019.win2020.st@gmail.com>
2022-07-24 21:20:29 +0900

    update: version v1.7.4

--- src/components/block/Version.tsx
+++ src/components/block/Version.tsx
@@ -4,7 +4,7 @@ import { Box, Typography } from "@mui/material";
 const Version = () => {
   return (
     <Box>
-      <Typography variant="caption">ver. 1.7.3 (22/07/23)</Typography>
+      <Typography variant="caption">ver. 1.7.4 (22/07/24)</Typography>
       <br />
       <Typography variant="caption">© 栄東祭実行委員会 技術部</Typography>
     </Box>
