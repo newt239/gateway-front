8cf7250bbcc45f0dab04bcb197e164999c8ad8f8
newt239 <ysti2019.win2020.st@gmail.com>
2022-08-20 00:02:28 +0900

    improve: password autocomplete warning at login page

--- src/components/page/Login.tsx
+++ src/components/page/Login.tsx
@@ -85,107 +85,106 @@ const Login = () => {
     }
   };
   return (
-    <>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Card variant="outlined" sx={{ p: 2, height: "100%" }}>
-            <Grid container spacing={2} sx={{ p: 2 }}>
-              {errorMessage && (
-                <Grid item xs={12}>
-                  <Alert severity="error" variant="filled">
-                    {errorMessage}
-                  </Alert>
-                </Grid>
-              )}
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Card variant="outlined" sx={{ p: 2, height: "100%" }}>
+          <Grid container spacing={2} sx={{ p: 2 }}>
+            {errorMessage && (
               <Grid item xs={12}>
-                {loading && <LinearProgress />}
+                <Alert severity="error" variant="filled">
+                  {errorMessage}
+                </Alert>
               </Grid>
-              <Grid item xs={12} md={6}>
-                <TextField
-                  id="user_id"
-                  label="ユーザーID"
-                  type="text"
-                  onChange={(event) => setUserIdValue(event.target.value)}
-                  fullWidth
-                />
-              </Grid>
-              <Grid item xs={12} md={6}>
-                <TextField
-                  id="password"
-                  label="パスワード"
-                  type="password"
-                  onChange={(event) => setPasswordValue(event.target.value)}
-                  onKeyPress={(e) => {
-                    if (e.key === "Enter") {
-                      login();
-                    }
-                  }}
-                  fullWidth
-                />
-              </Grid>
-              <Grid item xs={12} sx={{ textAlign: "right" }}>
-                <Button
-                  onClick={login}
-                  onKeyPress={(e) => {
-                    if (e.key === "Enter") {
-                      login();
-                    }
-                  }}
-                  variant="outlined"
-                  size="large"
-                  startIcon={<LoginRoundedIcon />}
-                >
-                  ログイン
-                </Button>
-              </Grid>
+            )}
+            <Grid item xs={12}>
+              {loading && <LinearProgress />}
             </Grid>
-          </Card>
-        </Grid>
+            <Grid item xs={12} md={6}>
+              <TextField
+                id="user_id"
+                label="ユーザーID"
+                type="text"
+                onChange={(event) => setUserIdValue(event.target.value)}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <TextField
+                id="password"
+                label="パスワード"
+                type="password"
+                autoComplete="current-password"
+                onChange={(event) => setPasswordValue(event.target.value)}
+                onKeyPress={(e) => {
+                  if (e.key === "Enter") {
+                    login();
+                  }
+                }}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sx={{ textAlign: "right" }}>
+              <Button
+                onClick={login}
+                onKeyPress={(e) => {
+                  if (e.key === "Enter") {
+                    login();
+                  }
+                }}
+                variant="outlined"
+                size="large"
+                startIcon={<LoginRoundedIcon />}
+              >
+                ログイン
+              </Button>
+            </Grid>
+          </Grid>
+        </Card>
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <Card variant="outlined" sx={{ p: 2, height: "100%" }}>
+          <Typography variant="h2">ログイン出来ない場合</Typography>
+          <ol>
+            <li>
+              ユーザーIDとパスワードが間違っていないかもう一度確認してください。
+            </li>
+            <li>
+              「サーバーからの応答がありません」というエラーが表示された場合、端末のネットワークの設定を確認した上で
+              <Link
+                href={process.env.REACT_APP_STATUS_URL || "/"}
+                target="_blank"
+                underline="hover"
+              >
+                サーバーステータス
+              </Link>
+              に異常がないか確認してください。
+            </li>
+            <li>ログイン状態は一定の時間が経過するとログアウトされます。</li>
+          </ol>
+        </Card>
+      </Grid>
+      {!window.matchMedia("(display-mode: standalone)").matches && (
         <Grid item xs={12} md={6}>
           <Card variant="outlined" sx={{ p: 2, height: "100%" }}>
-            <Typography variant="h2">ログイン出来ない場合</Typography>
+            <Typography variant="h2">アプリのインストール方法</Typography>
             <ol>
               <li>
-                ユーザーIDとパスワードが間違っていないかもう一度確認してください。
+                右上の <MoreVertIcon sx={{ verticalAlign: -5 }} />{" "}
+                から『「Gateway」をインストール』をクリック
               </li>
               <li>
-                「サーバーからの応答がありません」というエラーが表示された場合、端末のネットワークの設定を確認した上で
-                <Link
-                  href={process.env.REACT_APP_STATUS_URL || "/"}
-                  target="_blank"
-                  underline="hover"
-                >
-                  サーバーステータス
-                </Link>
-                に異常がないか確認してください。
+                Safariを利用している場合{" "}
+                <IosShareIcon sx={{ verticalAlign: -5 }} />{" "}
+                から「ホーム画面に追加」をタップ
               </li>
-              <li>ログイン状態は一定の時間が経過するとログアウトされます。</li>
+              <li>
+                ホーム画面に追加された「Gateway」アイコンをタップして起動
+              </li>
             </ol>
           </Card>
         </Grid>
-        {!window.matchMedia("(display-mode: standalone)").matches && (
-          <Grid item xs={12} md={6}>
-            <Card variant="outlined" sx={{ p: 2, height: "100%" }}>
-              <Typography variant="h2">アプリのインストール方法</Typography>
-              <ol>
-                <li>
-                  右上の <MoreVertIcon sx={{ verticalAlign: -5 }} />{" "}
-                  から『「Gateway」をインストール』をクリック
-                </li>
-                <li>
-                  Safariを利用している場合{" "}
-                  <IosShareIcon sx={{ verticalAlign: -5 }} />{" "}
-                  から「ホーム画面に追加」をタップ
-                </li>
-                <li>
-                  ホーム画面に追加された「Gateway」アイコンをタップして起動
-                </li>
-              </ol>
-            </Card>
-          </Grid>
-        )}
-      </Grid>
-    </>
+      )}
+    </Grid>
   );
 };
 
