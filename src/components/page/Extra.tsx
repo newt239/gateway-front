3f604a621c12eb1b6ee0ce8de9f2d95bf8e2224d
newt239 <ysti2019.win2020.st@gmail.com>
2022-08-21 15:44:10 +0900

    add: logout button to Extra component

--- src/components/page/Extra.tsx
+++ src/components/page/Extra.tsx
@@ -1,59 +1,75 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSetAtom } from "jotai";
-import { pageTitleAtom } from "#/components/lib/jotai";
+import { pageTitleAtom, profileAtom, tokenAtom } from "#/components/lib/jotai";
 
-import { Grid, Card, Box, Typography, Button } from "@mui/material";
+import { Card, Box, Typography, Button } from "@mui/material";
+import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
 
 type extraProp = {
   type: "404" | "401" | "unknown" | "loading";
 };
 
 const NotFound = (props: extraProp) => {
-  const navigate = useNavigate();
   const setPageTitle = useSetAtom(pageTitleAtom);
+  const navigate = useNavigate();
+  const setProfile = useSetAtom(profileAtom);
+  const setToken = useSetAtom(tokenAtom);
 
   useEffect(() => {
     if (props.type !== "loading") {
       setPageTitle("エラー");
     }
   }, []);
+
+  const logout = () => {
+    setToken(null);
+    setProfile(null);
+    localStorage.removeItem("gatewayApiToken");
+    navigate("/login", { replace: true });
+  };
+
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={12}>
-        <Card variant="outlined" sx={{ p: 2, height: "100%" }}>
-          {props.type === "404" ? (
-            <Typography>お探しのページは見つかりませんでした。</Typography>
-          ) : props.type === "401" ? (
-            <Typography>このページを表示する権限がありません。</Typography>
-          ) : props.type === "unknown" ? (
-            <>
-              <Typography>
-                このアカウントは使えません。テスト用のアカウントである可能性があります。
-              </Typography>
-              <Typography>
-                団体の代表者へメールでアカウントを配布しています。自分の団体に配られているアカウントを使用してください。
-              </Typography>
-              <Typography>
-                メールを受け取っていない場合は技術部までお問い合わせください。
-              </Typography>
-            </>
-          ) : props.type === "loading" ? (
-            <Typography>ログインセッションの検証中...</Typography>
-          ) : (
-            <Typography>何らかのエラーが発生しました。</Typography>
-          )}
-          <Box sx={{ textAlign: "right" }}>
-            <Button
-              onClick={() => navigate("/", { replace: true })}
-              variant="outlined"
-            >
-              トップに戻る
-            </Button>
-          </Box>
-        </Card>
-      </Grid>
-    </Grid>
+    <Card variant="outlined" sx={{ p: 2, height: "100%" }}>
+      {props.type === "404" ? (
+        <Typography>お探しのページは見つかりませんでした。</Typography>
+      ) : props.type === "401" ? (
+        <Typography>このページを表示する権限がありません。</Typography>
+      ) : props.type === "unknown" ? (
+        <>
+          <Typography>
+            このアカウントは使えません。テスト用のアカウントである可能性があります。
+          </Typography>
+          <Typography>
+            団体の代表者へメールでアカウントを配布しています。自分の団体に配られているアカウントを使用してください。
+          </Typography>
+          <Typography>
+            メールを受け取っていない場合は技術部までお問い合わせください。
+          </Typography>
+        </>
+      ) : props.type === "loading" ? (
+        <Typography>ログインセッションの検証中...</Typography>
+      ) : (
+        <Typography>何らかのエラーが発生しました。</Typography>
+      )}
+      <Box sx={{ mt: 2, textAlign: "right" }}>
+        <Button
+          variant="outlined"
+          color="error"
+          onClick={logout}
+          startIcon={<LogoutRoundedIcon />}
+          sx={{ mr: 2 }}
+        >
+          ログアウト
+        </Button>
+        <Button
+          onClick={() => navigate("/", { replace: true })}
+          variant="outlined"
+        >
+          トップに戻る
+        </Button>
+      </Box>
+    </Card>
   );
 };
 
