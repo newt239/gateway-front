a9d127af5e7ee5db3a8fc2b7df9172229788603f
newt239 <ysti2019.win2020.st@gmail.com>
2022-08-21 17:05:21 +0900

    fix: didn't show app version on amplify

--- src/components/block/Version.tsx
+++ src/components/block/Version.tsx
@@ -4,7 +4,7 @@ import { Box, Typography } from "@mui/material";
 const Version = () => {
   return (
     <Box>
-      <Typography variant="caption">ver. {process.env.REACT_APP_VERSION}-{process.env.REACT_APP_ENV} (22/08/20)</Typography>
+      <Typography variant="caption">ver. 1.8.6-{process.env.REACT_APP_ENV} (22/08/20)</Typography>
       <br />
       <Typography variant="caption">© 栄東祭実行委員会 技術部</Typography>
     </Box>
