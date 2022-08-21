ccd96611070a181d9e8d4948419a64aa3ef267e4
newt239 <ysti2019.win2020.st@gmail.com>
2022-08-21 17:20:17 +0900

    update: version 1.8.7

--- src/components/block/Version.tsx
+++ src/components/block/Version.tsx
@@ -5,7 +5,7 @@ const Version = () => {
   return (
     <Box>
       <Typography variant="caption">
-        ver. 1.8.6-{process.env.REACT_APP_ENV} (22/08/20)
+        ver. 1.8.7-{process.env.REACT_APP_ENV} (22/08/20)
       </Typography>
       <br />
       <Typography variant="caption">© 栄東祭実行委員会 技術部</Typography>
