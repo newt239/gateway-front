16d53777271d4dfc9f1472aaba83cadf6f316fa9
newt239 <ysti2019.win2020.st@gmail.com>
2022-08-25 19:54:23 +0900

    update:version 1.9.1

--- src/components/block/Version.tsx
+++ src/components/block/Version.tsx
@@ -5,7 +5,7 @@ const Version: React.VFC = () => {
   return (
     <Box>
       <Typography variant="caption">
-        ver. 1.8.7-{process.env.REACT_APP_ENV} (22/08/20)
+        ver. 1.9.1-{process.env.REACT_APP_ENV} (22/08/25)
       </Typography>
       <br />
       <Typography variant="caption">© 栄東祭実行委員会 技術部</Typography>
