861d72d50adec7bbf9ae6dae2b0799d68a722358
newt239 <ysti2019.win2020.st@gmail.com>
2022-08-28 00:50:27 +0900

    fix: can't get exhibit id at analytics page with exhibit account

--- src/components/page/Body.tsx
+++ src/components/page/Body.tsx
@@ -119,25 +119,19 @@ const Body: React.VFC = () => {
                   )}
                 </Route>
                 <Route path="analytics">
-                  {["moderator"].includes(profile.user_type) ? (
+                  {["moderator", "exhibit"].includes(profile.user_type) && (
+                    <Route
+                      path="exhibit/:exhibitId"
+                      element={<AnalyticsExhibit />}
+                    />
+                  )}
+                  {["moderator"].includes(profile.user_type) && (
                     <>
                       <Route index element={<AnalyticsIndex />} />
-                      <Route
-                        path="exhibit/:exhibitId"
-                        element={<AnalyticsExhibit />}
-                      />
                       <Route path="summary" element={<AnalyticsSummary />} />
                     </>
-                  ) : ["exhibit"].includes(profile.user_type) ? (
-                    <>
-                      <Route
-                        path={`exhibit/${profile.user_id}`}
-                        element={<AnalyticsExhibit />}
-                      />
-                    </>
-                  ) : (
-                    <Route path="*" element={<Extra type="notFound" />} />
                   )}
+                  <Route path="*" element={<Extra type="notFound" />} />
                 </Route>
                 <Route path="admin">
                   {["moderator"].includes(profile.user_type) ? (
