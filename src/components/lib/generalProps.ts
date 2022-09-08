005fda3c8bed3dbfe4453ce9fb11f17190463fde
newt239 <ysti2019.win2020.st@gmail.com>
2022-09-09 08:27:40 +0900

    update:version 1.9.6

--- src/components/lib/generalProps.ts
+++ src/components/lib/generalProps.ts
@@ -1,6 +1,6 @@
 const generalProps = {
-  app_version: "1.9.5",
-  version_date: "2022/08/31",
+  app_version: "1.9.6",
+  version_date: "2022/09/09",
   time_part: [
     {
       part_name: "全時間帯",
